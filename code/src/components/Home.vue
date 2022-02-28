7c58af7af922e8b8e311a2d062a66e8536392f2c
ChenXander <cx1197257655@gmail.com>
2022-02-28 20:25:50 +0800

    主页基本布局

--- code/src/components/Home.vue
+++ code/src/components/Home.vue
@@ -1,20 +1,41 @@
 <template>
-  <div>
-      <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header
+      >Header<el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-    methods: {
-        logout(){
-            window.sessionStorage.clear()
-            this.$router.push('/login')
-        }
-    }
-}
+  methods: {
+    logout() {
+      window.sessionStorage.clear();
+      this.$router.push("/login");
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
-
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
