a06712c6fb9fd93d6c0bc1126c8e2ee838d73a1b
ChenXander <cx1197257655@gmail.com>
2022-02-28 20:00:42 +0800

    退出登录清空token

--- code/src/components/Login.vue
+++ code/src/components/Login.vue
@@ -6,19 +6,32 @@
         <img src="../assets/logo.png" alt="" />
       </div>
       <!-- 登录表单区域 -->
-      <el-form :model="loginForm" label-width="0px" class="login_form">
-          <!-- 用户名 -->
-        <el-form-item>
-          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginFormRules"
+        label-width="0px"
+        class="login_form"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="iconfont icon-user"
+          ></el-input>
         </el-form-item>
         <!-- 密码 -->
-        <el-form-item>
-          <el-input type="password" v-model="loginForm.password" prefix-icon="iconfont icon-3702mima"></el-input>
+        <el-form-item prop="password">
+          <el-input
+            type="password"
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+          ></el-input>
         </el-form-item>
         <!-- 按钮 -->
         <el-form-item class="btns">
-          <el-button type="primary">登录</el-button>
-          <el-button type="info">重置</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button @click="resetLoginForm" type="info">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -27,15 +40,60 @@
 
 <script>
 export default {
-    data() {
-        return {
-            // 登录表单的绑定对象
-            loginForm: {
-                username: 'zs',
-                password: '123'
-            }
-        }
-    }
+  data() {
+    return {
+      // 登录表单的绑定对象
+      loginForm: {
+        username: "admin",
+        password: "123456",
+      },
+      // 表单的验证规则对象
+      loginFormRules: {
+        // 验证用户名
+        username: [
+          { required: true, message: "请输入登录名称", trigger: "blur" },
+          {
+            min: 3,
+            max: 10,
+            message: "长度在 3 到 10 个字符",
+            trigger: "blur",
+          },
+        ],
+        // 验证密码
+        password: [
+          { required: true, message: "请输入登录密码", trigger: "blur" },
+          {
+            min: 6,
+            max: 15,
+            message: "长度在 6 到 15 个字符",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    //   重置按钮，重置表单
+    resetLoginForm() {
+      this.$refs.loginFormRef.resetFields();
+    },
+    // 登录按钮，表单预验证
+    login() {
+      this.$refs.loginFormRef.validate(async (valid) => {
+        if (!valid) return;
+        const { data: res } = await this.$http.post("login", this.loginForm);
+        if (res.meta.status !== 200) return this.$message.error("登陆失败");
+        this.$message.success("登录成功");
+        /* 将登录成功后的token保存到客户端的sessionStorage中
+            项目中除了登录之外的其他api接口，必须在登录之后才能访问
+            token只应在当前网站打开期间生效，所以将token保存在sessionStorage中
+            通过编程式导航跳转到后台主页，路由地址是/home
+        */
+       window.sessionStorage.setItem('token', res.data.token);
+       this.$router.push('/home');
+      });
+    },
+  },
 };
 </script>
 
@@ -76,15 +134,15 @@ export default {
 }
 
 .login_form {
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    padding: 0 20px;
-    box-sizing: border-box;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
 }
 
 .btns {
-    display: flex;
-    justify-content: flex-end;
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
--- code/src/router/index.js
+++ code/src/router/index.js
@@ -1,16 +1,35 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+// 登录组件
 import Login from "../components/Login.vue";
+// 主页组件
+import Home from "../components/Home.vue";
+
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: "/", redirect: "/login" },
   { path: "/login", component: Login },
+  { path: "/home", component: Home },
+
 ];
 
 const router = new VueRouter({
   routes,
 });
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+    // to 将要访问的路径
+    // from 代表从哪个路径跳转而来
+    // next 放行函数 next('/login)可以强制跳转指定页面
+
+    if(to.path === '/login') return next();
+    // 获取token
+    const tokenStr = window.sessionStorage.getItem('token')
+    if(!tokenStr) return next('/login');
+    next();
+})
 
 export default router;
