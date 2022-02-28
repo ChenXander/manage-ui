6d509c54948ffcaf9b8da7eaab9f5cf343d70ce4
ChenXander <cx1197257655@gmail.com>
2022-02-28 19:03:13 +0800

    登录表单数据绑定

--- code/src/components/Login.vue
+++ code/src/components/Login.vue
@@ -1,15 +1,42 @@
 <template>
   <div class="login_container">
     <div class="login_box">
+      <!-- 头像区域 -->
       <div class="avatar_box">
         <img src="../assets/logo.png" alt="" />
       </div>
+      <!-- 登录表单区域 -->
+      <el-form :model="loginForm" label-width="0px" class="login_form">
+          <!-- 用户名 -->
+        <el-form-item>
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item>
+          <el-input type="password" v-model="loginForm.password" prefix-icon="iconfont icon-3702mima"></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            // 登录表单的绑定对象
+            loginForm: {
+                username: 'zs',
+                password: '123'
+            }
+        }
+    }
+};
 </script>
 
 <style lang="less" scoped>
@@ -46,5 +73,18 @@ export default {};
     border-radius: 50%;
     background-color: #eee;
   }
+}
+
+.login_form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+
+.btns {
+    display: flex;
+    justify-content: flex-end;
 }
 </style>
