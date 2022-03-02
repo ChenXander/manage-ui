6d93cabdfe49fe4b9ce9766372bf4662bd23078b
ChenXander <cx1197257655@gmail.com>
2022-03-02 08:10:13 +0800

    完成用户列表相关功能

--- code/src/plugins/element.js
+++ code/src/plugins/element.js
@@ -23,6 +23,7 @@ import {
   Tooltip,
   Pagination,
   Dialog,
+  MessageBox
 } from "element-ui";
 
 Vue.use(Button);
@@ -49,3 +50,4 @@ Vue.use(Pagination);
 Vue.use(Dialog);
 
 Vue.prototype.$message = Message;
+Vue.prototype.$confirm = MessageBox.confirm;
